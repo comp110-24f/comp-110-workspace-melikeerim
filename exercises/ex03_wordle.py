dbde0c44b96747557e6ac14e349fb927e126fa12
Melike Erim <melikeer@unc.edu>
2024-09-28 18:53:41 -0400

    working on ex03

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -4,14 +4,21 @@ __author__ = "730756528"
 
 
 def input_guess(secret_word_len: int) -> str:
+    """Checking if the word given by the player has the same length with secret word"""
     guess: str = input(f"Enter a {secret_word_len} character word: ")
-    while True:
-        if (len(guess) < secret_word_len) or (len(guess) > secret_word_len):
-            print(f" That wasn't {secret_word_len} chars!")
-            guess = input("Try again: ")
+    while not len(guess) == secret_word_len:
+        print(f" That wasn't {secret_word_len} chars! Try again: ")
+        guess = input()
+    print(guess)
+    return guess
+
+
+def contains_char(secret_word: str, char_guessed: str) -> bool:
+    """Looking for the character"""
+    assert len(char_guessed) == 1
+    index: int = 0
+    while index < len(secret_word):
+        if secret_word[index] == char_guessed:
+            return True
         else:
-            print(guess)
-            return guess
-
-
-input_guess(secret_word_len=5)
+            index = index + 1
